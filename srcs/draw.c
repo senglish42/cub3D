962cc78df6d778c0ad6b692f4baa4ae5d7d4b86b
trazumov <trazumov@list.ru>
2022-02-25 20:36:39 +0300

    Norminette 1 step

--- srcs/draw.c
+++ srcs/draw.c
@@ -1,31 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svirgil <svirgil@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 19:48:57 by svirgil           #+#    #+#             */
+/*   Updated: 2022/02/25 20:35:31 by svirgil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3D.h"
 
 void	my_mlx_pixel_put(t_img *image, int x, int y, int color)
 {
 	char	*dst;
+	int		err;
 
-	int ERR = 0;
-	if (x < 0)	ERR = 1 ;
-	if (y < 0)	ERR = 1 ;
-	if (x >= image->screen_w)	ERR = 1 ;
-	if (y >= image->screen_h)	ERR = 1 ;
-	if (ERR)
-	{
-//		printf("Error - trying to write to mem: x =%d y = %d %d %d\n", x ,y,
-//			   image->screen_w, image->screen_h);
+	err = 0;
+	if (x < 0)
+		err = 1 ;
+	if (y < 0)
+		err = 1 ;
+	if (x >= image->screen_w)
+		err = 1 ;
+	if (y >= image->screen_h)
+		err = 1 ;
+	if (err)
 		return ;
-	}
-	dst = (char *)image->addr + (y * image->line_length + x *
-			(image->bits_per_pixel /
-			8));
-	*(unsigned int*)dst = color;
+	dst = (char *)image->addr + \
+	(y * image->line_length + x * (image->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
 void	my_clear_window(t_game *game)
 {
-	int i = 0;
-	int j;
+	int	i;
+	int	j;
 
+	i = 0;
 	while (i < game->image.screen_w)
 	{
 		j = 0;
@@ -40,9 +53,11 @@ void	my_clear_window(t_game *game)
 
 void	draw_quad(t_game *game, int x, int y, int x0, int y0, int color)
 {
-	int i = y;
-	int j = x;
+	int	i;
+	int	j;
 
+	i = y;
+	j = x;
 	while (i < y0)
 	{
 		j = x;
@@ -55,36 +70,48 @@ void	draw_quad(t_game *game, int x, int y, int x0, int y0, int color)
 	}
 }
 
+static int	player_or_space(char c)
+{
+	if (c == '0' || c == 'N' || c == 'W' || c == 'E' || c == 'S')
+		return (TRUE);
+	return (FALSE);
+}
+
 void	draw_minimap(t_game *game)
 {
-	int posx = 0;
-	int posy = 0;
-	int size = 10;
+	int	posx;
+	int	posy;
+	int	size;
 
+	posx = 0;
+	posy = 0;
+	size = 8;
 	while (posy < game->map.height)
 	{
 		posx = -1;
 		while (++posx < game->map.width)
 		{
-			if (game->map.size[posy][posx] == '1')
-				draw_quad(game, posx * size, posy * size, (posx + 1)
-				* size, (posy + 1) * size, BLACK);
+			if (player_or_space(game->map.size[posy][posx]))
+				draw_quad(game, posx * size, posy * size, \
+				(posx + 1) * size, (posy + 1) * size, BLACK);
 		}
 		posy++;
 	}
 }
 
-void draw_scaled_point(t_game *game, double x, double y, int color)
+void	draw_scaled_point(t_game *game, double x, double y, int color)
 {
-	int i = 0;
-	int j = 0;
+	int	i;
+	int	j;
 
+	i = 0;
+	j = 0;
 	while (i < game->map.scale)
 	{
 		j = 0;
 		while (j < game->map.scale)
 		{
-			my_mlx_pixel_put(&game->image, 10 * x + i, 10 * y + j, color);
+			my_mlx_pixel_put(&game->image, 8 * x + i, 8 * y + j, color);
 			j++;
 		}
 		i++;
@@ -93,86 +120,20 @@ void draw_scaled_point(t_game *game, double x, double y, int color)
 
 void	draw_miniplayer(t_game *game)
 {
-	double	x1;
-	double	y1;
-	double	num;
-	double	dx;
-	double	dy;
-	int		count;
-	double	min;
-	double 	max;
-	int 	cnt;
-	int		size = 10;
-
-	cnt = 0;
-	count = -1;
-	min = game->player.da - PI / 6;
-	max = game->player.da + PI / 6;
-	num = - 45 * PI / 180;
-
-	while (++count != 450)
-	{
-		if ((num >= min && num <= max))
-		{
-			dx = cos(num);
-			dy = sin(num);
-			game->player.disx[cnt] = 100000000;
-			x1 = game->player.posx * size;
-			y1 = game->player.posy * size;
-			while (game->map.size[(int)
-			(y1 / size)][(int)(x1/ size)]!= '1')
-			{
-				x1 = x1 + dx;
-				y1 = y1 - dy;
-			}
-			if ((int)(x1 + 1) % size == 0)
-				x1 = (int)(x1 + 1);
-			else if ((int)(x1 - 1) % size == 0)
-				x1 = (int)(x1 - 1);
-			else
-				x1 = (int)x1;
-			if ((int)(y1 + 1) % size == 0)
-				y1 = (int)(y1 + 1);
-			else if ((int)(y1 - 1) % size == 0)
-				y1 = (int)(y1 - 1);
-			else
-				y1 = (int)y1;
-			if ((int)(num * 100) == (int)(game->player.da * 100))
-				draw_line(game, game->player.posx * size, game->player.posy * size,
-						  x1,y1, GREEN);
-			else
-				draw_line(game, game->player.posx * size, game->player.posy *
-														size, x1,y1,RED);
-			if (count == 0)
-				game->player.mina = num;
-			else
-				game->player.maxa = num;
-			game->player.ang[cnt] = num;
-			game->player.angx[cnt] = x1 / size;
-			game->player.angy[cnt] = y1 / size;
-			game->player.disx[cnt] = sqrt((game->player.posx - game->player
-					.angx[cnt]) * (game->player.posx - game->player
-					.angx[cnt]) + (game->player.posy - game->player
-							.angy[cnt]) * (game->player.posy - game->player
-									.angy[cnt]));
-			cnt++;
-		}
-		num += PI / 180;
-	}
 	draw_scaled_point(game, game->player.posx, game->player.posy, YELLOW);
 }
 
-void    draw_line(t_game *game, double x, double y, double x1, double y1, int color)
+void	draw_line(t_game *game, double x, double y, double x1, double y1, int color)
 {
-	double   deltax;
-	double   deltay;
-	int     max;
+	double	deltax;
+	double	deltay;
+	int		max;
 
 	deltax = x1 - x;
 	deltay = y1 - y;
-
 	max = MAX(ABS(deltax), ABS(deltay));
-	deltay /= max; deltax /= max;
+	deltay /= max;
+	deltax /= max;
 	while ((int)(x1 - x) || (int)(y1 - y))
 	{
 		my_mlx_pixel_put(&game->image, x, y, color);
--- srcs/ident.c
+++ srcs/ident.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ident.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: senglish <senglish@student.21-school.ru>   +#+  +:+       +#+        */
+/*   By: svirgil <svirgil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 10:59:45 by senglish          #+#    #+#             */
-/*   Updated: 2022/01/26 13:50:34 by senglish         ###   ########.fr       */
+/*   Updated: 2022/02/25 19:48:00 by svirgil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "cub3D.h"
 
 void	path_ident(char *orient[6])
 {
-	int 	fd[4];
-	char 	*path;
-	int     count;
-
+	int		fd[4];
+	char	*path;
+	int		count;
 
 	path = NULL;
 	count = -1;
@@ -33,76 +33,76 @@ void	path_ident(char *orient[6])
 		close(fd[count]);
 }
 
-void    to_compare(const char *str1, const char *str2, int errnum)
+void	to_compare(const char *str1, const char *str2, int errnum)
 {
-    size_t  len1;
-    size_t  len2;
+	size_t	len1;
+	size_t	len2;
 
-    len1 = ft_strlen(str1);
-    len2 = ft_strlen(str2);
-    if (len1 == len2)
-    {
-        if (!ft_strncmp(str1, str2, len1))
-            error(errnum);
-    }
+	len1 = ft_strlen(str1);
+	len2 = ft_strlen(str2);
+	if (len1 == len2)
+	{
+		if (!ft_strncmp(str1, str2, len1))
+			error(errnum);
+	}
 }
 
-void    compare_ident(char *orient[6])
+void	compare_ident(char *orient[6])
 {
-    int     num;
-    int     count;
+	int	num;
+	int	count;
 
-    count = -1;
-    while (++count < 3)
-    {
+	count = -1;
+	while (++count < 3)
+	{
 		num = -1;
-        while (++num <= 3)
-        {
-            if (num <= count)
-                continue;
-            to_compare(orient[count], orient[num], 11);
-        }
-    }
+		while (++num <= 3)
+		{
+			if (num <= count)
+				continue ;
+			to_compare(orient[count], orient[num], 11);
+		}
+	}
 }
 
-void 	fill_ident(char **turn, const char *orient, const char *str, int no)
+void	fill_ident(char **turn, const char *orient, const char *str, int no)
 {
-	int a;
+	int	a;
 
 	a = no - 1;
 	while (*(str + a) == ' ')
 		a++;
 	if (!ft_strncmp(str, orient, no))
-    {
-        if (!*turn)
-            *turn = (char *) (str + a);
-        else
-            error(9);
-    }
+	{
+		if (!*turn)
+			*turn = (char *)(str + a);
+		else
+			error(9);
+	}
 	else
 		error(10);
 	if (no == 3 && ft_strncmp(*turn, "./", 2))
 		error(10);
 }
 
-void    check_ident(t_game *game, short height, short width)
+void	check_ident(t_game *game, short height, short width)
 {
-    const char  *orient[6] = {"NO ", "SO ", "WE ", "EA ", "F ", "C "};
-    char        *str;
+	const char	*orient[6] = {"NO ", "SO ", "WE ", "EA ", "F ", "C "};
+	char		*str;
 
-    str = &game->parse[height][width];
-    if (*str == 'N')
-        fill_ident(&game->ident.orient[0], orient[0], str, 3);
-    else if (*str == 'S')
-        fill_ident(&game->ident.orient[1], orient[1], str, 3);
-    else if (*str == 'W')
-        fill_ident(&game->ident.orient[2], orient[2], str, 3);
-    else if (*str == 'E')
-        fill_ident(&game->ident.orient[3], orient[3], str, 3);
-    else if (*str == 'F')
-        fill_ident(&game->ident.orient[4], orient[4], str, 2);
-    else if (*str == 'C')
-        fill_ident(&game->ident.orient[5], orient[5], str, 2);
-    else
-        error(10);
+	str = &game->parse[height][width];
+	if (*str == 'N')
+		fill_ident(&game->ident.orient[0], orient[0], str, 3);
+	else if (*str == 'S')
+		fill_ident(&game->ident.orient[1], orient[1], str, 3);
+	else if (*str == 'W')
+		fill_ident(&game->ident.orient[2], orient[2], str, 3);
+	else if (*str == 'E')
+		fill_ident(&game->ident.orient[3], orient[3], str, 3);
+	else if (*str == 'F')
+		fill_ident(&game->ident.orient[4], orient[4], str, 2);
+	else if (*str == 'C')
+		fill_ident(&game->ident.orient[5], orient[5], str, 2);
+	else
+		error(10);
 }
